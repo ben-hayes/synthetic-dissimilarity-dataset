8767a6dc078d722dc7898fc4d39d2a3212d8fbe8
Ben Hayes <b.j.hayes@qmul.ac.uk>
2021-12-03 17:06:09 +0000

    Setup basic project

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name="SynDissim",
+    version="0.0.1",
+    description="Synthetic datasets for meta-modelling of audio dissimilarity",
+    author="Ben Hayes",
+    author_email="b.j.hayes@qmul.ac.uk",
+    url="https://github.com/ben-hayes/synthetic-dissimilarity-dataset",
+    packages=["syndissim"],
+)
